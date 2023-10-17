c6eadc3a355d52769be871a47d91c93ec055c60c
João Marcos <80593996+Joao-Sant-Ana@users.noreply.github.com>
2023-10-17 10:12:26 -0300

    correções
    - Correção de responsividade na pagina home.
    
    - Correção do menu de navegação em dispositivos móveis.

--- js/menuAnimation.js
+++ js/menuAnimation.js
@@ -1,4 +1,5 @@
 const $menuAbrir = document.getElementById('menuAbrir')
+const $logo = document.getElementById('logo')
 
 count = 1
 
@@ -6,7 +7,9 @@ $menuAbrir.addEventListener('click', () => {
     count++
     if (count % 2 == 0) {
         document.getElementById('info_nav_menu').style.display = "flex"
+        $logo.style.opacity = "30%"
     } else {
         document.getElementById('info_nav_menu').style.display = "none"
+        $logo.style.opacity = "100%"
     }
 })
